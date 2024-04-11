b699cbd4ff7259e134230fc6fe9b58a4146768c5
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-04-11 15:25:34 +0900

    로그인 기능 로그인 에러 로그아웃 filter 생성

--- w2/src/main/java/org/zerock/w2/controller/TodoRegisterController.java
+++ w2/src/main/java/org/zerock/w2/controller/TodoRegisterController.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -24,6 +25,26 @@ public class TodoRegisterController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         log.info("/todo/register GET ......");
+
+        /* 로그인 정보 유무에  따른 register.jsp  이동 여부 결정 --- start */
+        HttpSession session = req.getSession();
+
+        if(session.isNew()){
+            log.info("이전에 접속한 정보가 없는 사용자 - JSESSIONID 쿠키가 새로 만들어진 사용자");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        // 접속한 적은 있지만, 로그인을 하지 않았다면
+        if(session.getAttribute("loginInfo") == null){
+            log.info("이전에 접속한 정보가 존재하는 사용자 - 로그인한 정보가 없는 사용자");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        /* 로그인 정보 유무에 따른 register.jsp 이동 여부 결정 --- end */
+
+        // 정상적인 경우라면 입려 화면으로
         req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, resp);
     }
 
