674cc0b4934dda6a397a4c4195f3c9fc87c2792a
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-04-19 14:54:17 +0900

    쿼리 메소드 querydsl 연동 DTO 구현 및 CRUD 기능 구현

--- b01/src/main/java/org/zerock/b01/repository/BoardRepository.java
+++ b01/src/main/java/org/zerock/b01/repository/BoardRepository.java
@@ -1,7 +1,11 @@
 package org.zerock.b01.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.zerock.b01.domain.Board;
+import org.zerock.b01.repository.search.BoardSearch;
 
 /* 인터페이스를 만들고
 JpaRepository를 상속받는데,
@@ -16,5 +20,21 @@ private BoardRepository boardRepository;
 
 나 생성자 주입을 통해서 사용하면 된다.
 *  */
-public interface BoardRepository  extends JpaRepository<Board, Long> {
+public interface BoardRepository  extends JpaRepository<Board, Long>, BoardSearch {
+
+    // 1) 쿼리메서드
+    // http://spring.io/projects/spring-data-jpa
+
+//    Page<Board> findTitleContainingOrderByBnoDesc(String keyword, Pageable pageable);
+//
+    // JPQL -  JPA에서 정한 표준 sql, jpa에서 사용하는 Hibernate에서 NativeQuery로 자동 변환해서
+    // DBMS에 전달한다.
+    @Query("select b from Board b where b.title like  concat('%',:keyword,'%')")
+    Page<Board> findKeyword(String keyword, Pageable pageable);
+
+    // MariaDB에 적합한 sql - NativeQuery
+    @Query(value = "select now()", nativeQuery = true)
+    String getTime();
 }
+
+    // 4) QueryDsl - 프로그래밍(객체 QueryDsl - 프로그래밍(객체, 메서드 호출) -> JPQL -> NativQuery)
---
+++ b01/src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
@@ -0,0 +1,117 @@
+package org.zerock.b01.repository.search;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.JPQLQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.zerock.b01.domain.Board;
+import org.zerock.b01.domain.QBoard;
+
+import java.util.List;
+
+// 반드시 상속받은 인터페이스명 + Impl
+public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardSearch {
+
+    public BoardSearchImpl() {
+        super(Board.class);
+    }
+
+    // 우리는 BoardRepostitory 내부에서 호출할 메서드를
+    // QueryDsl로 구현한다.
+    @Override
+    public Page<Board> search1(Pageable pageable) {
+
+        // 우리가 작업하는 querydsl 프로그램 -> jpql로 변환하기 위해 사용
+        QBoard board = QBoard.board; // q도메인 객체
+
+        /*
+        * querydsl을 통해서 1단계씩 sql문을 작성해 나간다.
+        * */
+
+        // from BOARD
+        JPQLQuery<Board> query = from(board); // select.. from board
+
+//        // WHERE title LIKE '%1%'
+//        query.where(board.title.contains("1")); // where title like...
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder(); // (
+
+        booleanBuilder.or(board.title.contains("11")); // title like ...
+
+        booleanBuilder.or(board.content.contains("11")); // content like ....
+
+        query.where(booleanBuilder);
+        query.where(board.bno.gt(0L));
+
+        // paging
+        this.getQuerydsl().applyPagination(pageable, query);
+
+        //select * from board where title like '%1%'
+        List<Board> list = query.fetch();
+
+        // select Count(bno) from board where title like '%1%'
+        long count = query.fetchCount();
+
+        return null;
+    }
+
+    @Override
+    public Page<Board> searchAll(String[] types, String keyword, Pageable pageable) {
+
+        // Querydsl -> JPQL 변환하기 위한 역할
+        QBoard board = QBoard.board;
+
+        // FROM board
+        JPQLQuery<Board> query = from(board);
+
+        if( (types != null && types.length > 0) && keyword != null ){ //검색 조건과 키워드가 있다면
+
+            BooleanBuilder booleanBuilder = new BooleanBuilder(); // (
+
+            /*
+            * title LIKE '%1%'
+            * OR content LIKE '%1%'
+            * OR writer LIKE '%1%'
+            * */
+
+            for(String type: types){
+
+                switch (type){
+                    case "t":
+                        booleanBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "c":
+                        booleanBuilder.or(board.content.contains(keyword));
+                        break;
+                    case "w":
+                        booleanBuilder.or(board.writer.contains(keyword));
+                        break;
+                }
+            }//end for
+
+            /*
+            * WHERE (
+            * title LIKE '%1%'
+            * OR content LIKE '%1%'
+            * OR writer LIKE '%1%'
+            * )
+            * */
+            query.where(booleanBuilder);
+        }//end if
+
+        // AND bno > 0
+        query.where(board.bno.gt(0L));
+
+        //paging
+        this.getQuerydsl().applyPagination(pageable, query);
+
+        List<Board> list = query.fetch();
+
+        long count = query.fetchCount();
+
+        return new PageImpl<>(list, pageable, count);
+
+    }
+}
