8ba6f0d6b2acb17b7f89f35f18d963b1849bfdcb
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-04-26 14:35:01 +0900

    게시물 첨부파일 수정 듀플 처리 게시물 수정 목록 수정 삭제 조회 처리

--- b01/src/main/java/org/zerock/b01/repository/ReplyRepository.java
+++ b01/src/main/java/org/zerock/b01/repository/ReplyRepository.java
@@ -14,5 +14,29 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
     @Query("select r from Reply r where r.board.bno = :bno")
     Page<Reply> listOfBoard(Long bno, Pageable pageable);
 
+    /*
+    * JPA의 DML을 사용하기 위한 보편적 3가지 방법
+    *
+    * 모두 JPQL -> Native Query -> DBMS 서버에 전달
+    *
+    *  단순 ------------------- 복잡
+    * 쿼리메서드    JPQL        Querydsl
+    *
+    * 1) 쿼리 메서드
+    *    기본 CRUD는 자동으로 이미 만들어져 있다.
+    *    Repository 인터페이스 함수명명규칙에 따라 이름을 정하면,자동으로 JPA가 JPQL로 만들어준다.
+    * 2) JPQL
+    *    SQL문과 유사하다.
+    *    이것이 나온 이유는 DBMS마다 약간씩 SQL이 다르므로 JPA에서 사용하는 표준을 정해놓은 것.
+    *
+    * 3) Querydsl
+    *    조건이 복잡할 때, 메서드 호출방식으로 개발자가 조합하는게 익숙하도록 제공하는 방식
+    *    제어문이 필요해서
+    *    (ex. Oracle의 PL-SQL의 역할)
+    * */
+
+    /* DELETE FROM Reply r WHERE r.board_bno=:bno
+     * */
     void deleteByBoard_Bno(Long bno);
+
 }
